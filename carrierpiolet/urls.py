b9af9924da9a35aea9cbe85a01ba60e340093cd6
Anshul Verma <anshulverma1405@gmail.com>
2024-03-14 23:05:45 +0530

    bug fix: vercel deployment

--- carrierpiolet/urls.py
+++ carrierpiolet/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from scrapper.AppConstants import AppConstants
 from . import views
+from scrapper.AppConstants import AppConstants
 
 urlpatterns = [
     path('admin', admin.site.urls),
