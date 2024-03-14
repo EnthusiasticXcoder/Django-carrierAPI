21739246925d866fb2f6db76c947911a805e6dec
Anshul Verma <anshulverma1405@gmail.com>
2024-03-15 04:17:17 +0530

    update: credentials

--- carrierpiolet/settings.py
+++ carrierpiolet/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,7 +98,16 @@ REST_FRAMEWORK = {
 # }
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT'),
+        "CONN_MAX_AGE": 5,
+        "CONN_HEALTH_CHECKS": False,
+    }
 }
 
 
